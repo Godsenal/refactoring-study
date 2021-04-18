a9a5fa7b2b59d0c4dce11654a31f62d4e05b76b6
godsenal <tmqps78@gmail.com>
2021-04-19 01:30:01 +0900

    :recycle: invoice의 customer 데이터를 statementData로 이동

--- src/index.ts
+++ src/index.ts
@@ -2,13 +2,15 @@ import invoices from "./data/invoices.json";
 import plays from "./data/plays.json";
 
 function statement(invoice, plays) {
-  const statementData = {};
+  const statementData = {} as any;
+
+  statementData.customer = invoices.performances;
 
   return renderPlainText(statementData, invoice, plays);
 }
 
 function renderPlainText(data, invoice: any, plays: any) {
-  let result = `청구 내역 (고객명: ${invoice.customer})\n`;
+  let result = `청구 내역 (고객명: ${data.customer})\n`;
 
   for (let perf of invoice.performances) {
     result += `  ${playFor(perf).name}: ${usd(amountFor(perf))} (${
