5a1ef0cb93256d2319f25f2f65d20b7b30750709
godsenal <tmqps78@gmail.com>
2021-04-19 02:12:55 +0900

    :recycle: amount 구하는 로직 클래스로 인라인

--- src/createStatementData.ts
+++ src/createStatementData.ts
@@ -47,7 +47,7 @@ function createStatementData(invoice, plays) {
     );
     const result = Object.assign({}, aPerformance);
     result.play = calculator.play;
-    result.amount = amountFor(result);
+    result.amount = calculator.amount;
     result.volumeCredits = volumeCreditsFor(result);
 
     return result;
@@ -55,11 +55,6 @@ function createStatementData(invoice, plays) {
 
   function playFor(aPerformance) {
     return plays[aPerformance.playID];
-  }
-
-  function amountFor(aPerformance) {
-    return new PerformanceCalculator(aPerformance, playFor(aPerformance))
-      .amount;
   }
 
   function volumeCreditsFor(aPerformance) {
