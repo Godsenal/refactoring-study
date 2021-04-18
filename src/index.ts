1c6b4722ec068450f9c7781a11fe8c8a227295a8
godsenal <tmqps78@gmail.com>
2021-04-19 01:21:42 +0900

    :recycle: totalAmount도 함수로 쪼개기

--- src/index.ts
+++ src/index.ts
@@ -58,16 +58,24 @@ function statement(invoice: any, plays: any) {
 
     return volumeCredits;
   }
+  function appleSource() {
+    let totalAmount = 0;
 
-  let totalAmount = 0;
+    for (let perf of invoice.performances) {
+      totalAmount += amountFor(perf);
+    }
+
+    return totalAmount;
+  }
   let result = `청구 내역 (고객명: ${invoice.customer})\n`;
 
   for (let perf of invoice.performances) {
     result += `  ${playFor(perf).name}: ${usd(amountFor(perf))} (${
       perf.audience
     }석)\n`;
-    totalAmount += amountFor(perf);
   }
+
+  let totalAmount = appleSource();
 
   result += `총액: ${usd(totalAmount)}\n`;
   result += `적립 포인트: ${totalVolumeCredits()}점\n`;
