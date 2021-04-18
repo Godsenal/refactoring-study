2a7a22f0377610e32643c54c374da434ea1027a9
godsenal <tmqps78@gmail.com>
2021-04-19 01:37:15 +0900

    :recycle: amountFor도 statementData로 이동

--- src/index.ts
+++ src/index.ts
@@ -12,46 +12,13 @@ function statement(invoice, plays) {
   function enrichPerformance(aPerformance) {
     const result = Object.assign({}, aPerformance);
     result.play = playFor(result);
+    result.amount = amountFor(result);
 
     return result;
   }
 
   function playFor(aPerformance) {
     return plays[aPerformance.playID];
-  }
-}
-
-function renderPlainText(data, plays: any) {
-  let result = `청구 내역 (고객명: ${data.customer})\n`;
-
-  for (let perf of data.performances) {
-    result += `  ${perf.play.name}: ${usd(amountFor(perf))} (${
-      perf.audience
-    }석)\n`;
-  }
-
-  result += `총액: ${usd(totalAmount())}\n`;
-  result += `적립 포인트: ${totalVolumeCredits()}점\n`;
-
-  return result;
-
-  function totalVolumeCredits() {
-    let volumeCredits = 0;
-
-    for (let perf of data.performances) {
-      volumeCredits += volumeCreditsFor(perf);
-    }
-
-    return volumeCredits;
-  }
-  function totalAmount() {
-    let result = 0;
-
-    for (let perf of data.performances) {
-      result += amountFor(perf);
-    }
-
-    return result;
   }
 
   function amountFor(aPerformance) {
@@ -73,6 +40,38 @@ function renderPlainText(data, plays: any) {
         break;
       default:
         throw new Error(`알 수 없는 장르: ${aPerformance.play.type}`);
+    }
+
+    return result;
+  }
+}
+
+function renderPlainText(data, plays: any) {
+  let result = `청구 내역 (고객명: ${data.customer})\n`;
+
+  for (let perf of data.performances) {
+    result += `  ${perf.play.name}: ${usd(perf.amount)} (${perf.audience}석)\n`;
+  }
+
+  result += `총액: ${usd(totalAmount())}\n`;
+  result += `적립 포인트: ${totalVolumeCredits()}점\n`;
+
+  return result;
+
+  function totalVolumeCredits() {
+    let volumeCredits = 0;
+
+    for (let perf of data.performances) {
+      volumeCredits += volumeCreditsFor(perf);
+    }
+
+    return volumeCredits;
+  }
+  function totalAmount() {
+    let result = 0;
+
+    for (let perf of data.performances) {
+      result += perf.amount;
     }
 
     return result;
