97185dad5534fd158c94d763057aa88d71dbb559
godsenal <tmqps78@gmail.com>
2021-04-19 01:07:50 +0900

    :recycle: amountFor의 play도 playFor 함수 사용해서 가져오도록 수정

--- src/index.ts
+++ src/index.ts
@@ -8,7 +8,7 @@ function playFor(aPerformance) {
 function amountFor(aPerformance, play) {
   let result = 0;
 
-  switch (play.type) {
+  switch (playFor(aPerformance).type) {
     case "tragedy":
       result = 40000;
       if (aPerformance.audience > 30) {
@@ -23,7 +23,7 @@ function amountFor(aPerformance, play) {
       result += 300 * aPerformance.audience;
       break;
     default:
-      throw new Error(`알 수 없는 장르: ${play.type}`);
+      throw new Error(`알 수 없는 장르: ${playFor(aPerformance).type}`);
   }
 
   return result;
