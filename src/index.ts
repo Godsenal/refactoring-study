bef5cb2962c39a0ff347972057092d8e1a463758
godsenal <tmqps78@gmail.com>
2021-04-19 01:12:02 +0900

    :recycle: 변수 이름 수정

--- src/index.ts
+++ src/index.ts
@@ -29,15 +29,15 @@ function amountFor(aPerformance) {
   return result;
 }
 
-function volumeCreditsFor(perf) {
-  let volumeCredits = 0;
+function volumeCreditsFor(aPerformance) {
+  let result = 0;
 
-  volumeCredits += Math.max(perf.audience - 30, 0);
+  result += Math.max(aPerformance.audience - 30, 0);
 
-  if ("comedy" === playFor(perf).type)
-    volumeCredits += Math.floor(perf.audience / 5);
+  if ("comedy" === playFor(aPerformance).type)
+    result += Math.floor(aPerformance.audience / 5);
 
-  return volumeCredits;
+  return result;
 }
 
 function statement(invoice: any, plays: any) {
