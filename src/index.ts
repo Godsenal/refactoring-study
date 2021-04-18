562a6bacf3204f35b21a61eb4a6c0d66ac3440ee
godsenal <tmqps78@gmail.com>
2021-04-19 01:04:35 +0900

    :recycle: 변수 인라인하기

--- src/index.ts
+++ src/index.ts
@@ -40,14 +40,14 @@ function statement(invoice: any, plays: any) {
   }).format;
 
   for (let perf of invoice.performances) {
-    const play = playFor(perf);
-    let thisAmount = amountFor(perf, play);
+    let thisAmount = amountFor(perf, playFor(perf));
 
     volumeCredits += Math.max(perf.audience - 30, 0);
 
-    if ("comedy" === play.type) volumeCredits += Math.floor(perf.audience / 5);
+    if ("comedy" === playFor(perf).type)
+      volumeCredits += Math.floor(perf.audience / 5);
 
-    result += `  ${play.name}: ${format(thisAmount / 100)} (${
+    result += `  ${playFor(perf).name}: ${format(thisAmount / 100)} (${
       perf.audience
     }석)\n`;
     totalAmount += thisAmount;
