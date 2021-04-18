532505635206bb97435e071cf82a4915ac05ff54
godsenal <tmqps78@gmail.com>
2021-04-19 01:15:48 +0900

    :recycle: volumCredits 변수선언을 반복문 앞으로 이동

--- src/index.ts
+++ src/index.ts
@@ -50,7 +50,6 @@ function usd(aNumber: number) {
 
 function statement(invoice: any, plays: any) {
   let totalAmount = 0;
-  let volumeCredits = 0;
   let result = `청구 내역 (고객명: ${invoice.customer})\n`;
 
   for (let perf of invoice.performances) {
@@ -59,6 +58,9 @@ function statement(invoice: any, plays: any) {
     }석)\n`;
     totalAmount += amountFor(perf);
   }
+
+  let volumeCredits = 0;
+
   for (let perf of invoices.performances) {
     volumeCredits += volumeCreditsFor(perf);
   }
