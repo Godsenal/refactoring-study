61ff13dbb1973426d0c07c1cbf459dc253dd0d15
godsenal <tmqps78@gmail.com>
2021-04-27 00:24:45 +0900

    :white_check_mark: 각 테스트마다 모듈 리셋하도록 수정

--- src/chapter6/EncapsulateVariable.test.ts
+++ src/chapter6/EncapsulateVariable.test.ts
@@ -1,19 +1,26 @@
-import { defaultOwner } from "./defaultOwner";
-import example from "./EncapsulateVariable";
+describe("변수 캡슐화", () => {
+  let defaultOwner;
+  beforeEach(() => {
+    jest.resetModules();
+    defaultOwner = require("./defaultOwner").defaultOwner;
+  });
+  it("변수 캡슐화 테스트", () => {
+    const space = { owner: null };
 
-it("변수 캡슐화 테스트", () => {
-  const space = { owner: null };
+    require("./EncapsulateVariable").default(space);
 
-  example(space);
+    expect(space.owner).toStrictEqual({
+      firstName: "마틴",
+      lastName: "파울러",
+    });
+  });
 
-  expect(space.owner).toStrictEqual({ firstName: "마틴", lastName: "파울러" });
-});
+  it("필드 직접 변경 테스트", () => {
+    const owner1 = defaultOwner();
+    expect(owner1.lastName).toBe("파울러");
 
-it("필드 직접 변경 테스트", () => {
-  const owner1 = defaultOwner();
-  expect(owner1.lastName).toBe("파울러");
-
-  const owner2 = defaultOwner();
-  owner2.lastName = "파슨스";
-  expect(owner1.lastName).toBe("파울러");
+    const owner2 = defaultOwner();
+    owner2.lastName = "파슨스";
+    expect(owner1.lastName).toBe("파울러");
+  });
 });
