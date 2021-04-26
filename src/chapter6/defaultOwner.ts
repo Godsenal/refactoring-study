1862c405178cbe05c92c48683befe2719b218407
godsenal <tmqps78@gmail.com>
2021-04-27 00:25:42 +0900

    :white_check_mark: getter 가 데이터의 복제본을 반환하도록 수정
    - 변수에 담긴 내용을 변경하는 행위를 제어하고 싶을 때 적용

--- src/chapter6/defaultOwner.ts
+++ src/chapter6/defaultOwner.ts
@@ -1,5 +1,5 @@
 let defaultOwnerData = { firstName: "마틴", lastName: "파울러" };
 
-export const defaultOwner = () => defaultOwnerData;
+export const defaultOwner = () => ({ ...defaultOwnerData });
 export const setDefaultOwner = (arg: typeof defaultOwnerData) =>
   (defaultOwnerData = arg);
