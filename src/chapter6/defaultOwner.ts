d63a6af3fc6107d9a3d431f213ac0c0e97d8ebd0
godsenal <tmqps78@gmail.com>
2021-04-27 00:29:40 +0900

    :recycle: 레코드 캡슐화하기 적용
    - 레코드 캡슐화를 통해 변경 막기

--- src/chapter6/defaultOwner.ts
+++ src/chapter6/defaultOwner.ts
@@ -1,5 +1,20 @@
 let defaultOwnerData = { firstName: "마틴", lastName: "파울러" };
 
-export const defaultOwner = () => ({ ...defaultOwnerData });
+export const defaultOwner = () => new Person(defaultOwnerData);
 export const setDefaultOwner = (arg: typeof defaultOwnerData) =>
   (defaultOwnerData = arg);
+
+class Person {
+  private _lastName;
+  private _firstName;
+  constructor(data) {
+    this._lastName = data.lastName;
+    this._firstName = data.firstName;
+  }
+  get lastName() {
+    return this._lastName;
+  }
+  get firstName() {
+    return this._firstName;
+  }
+}
