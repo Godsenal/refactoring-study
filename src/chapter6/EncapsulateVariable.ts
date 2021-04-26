5723049c126e2c9ec2147d4b28ad6812f75a84cc
godsenal <tmqps78@gmail.com>
2021-04-27 00:14:11 +0900

    :recycle: getter 앞에 get prefix 제거

--- src/chapter6/EncapsulateVariable.ts
+++ src/chapter6/EncapsulateVariable.ts
@@ -1,7 +1,7 @@
-import { getDefaultOwner, setDefaultOwner } from "./defaultOwner";
+import { defaultOwner, setDefaultOwner } from "./defaultOwner";
 
 const example = (spaceship: { owner: any }) => {
-  spaceship.owner = getDefaultOwner();
+  spaceship.owner = defaultOwner();
 
   setDefaultOwner({ firstName: "레베카", lastName: "파슨스" });
 };
--- src/chapter6/defaultOwner.ts
+++ src/chapter6/defaultOwner.ts
@@ -1,5 +1,5 @@
-let defaultOwner = { firstName: "마틴", lastName: "파울러" };
+let defaultOwnerData = { firstName: "마틴", lastName: "파울러" };
 
-export const getDefaultOwner = () => defaultOwner;
-export const setDefaultOwner = (arg: typeof defaultOwner) =>
-  (defaultOwner = arg);
+export const defaultOwner = () => defaultOwnerData;
+export const setDefaultOwner = (arg: typeof defaultOwnerData) =>
+  (defaultOwnerData = arg);
