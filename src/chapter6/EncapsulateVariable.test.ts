dd1659d4ccbd9a7974149680ab13894a6aee5c4d
godsenal <tmqps78@gmail.com>
2021-04-27 00:18:47 +0900

    :white_check_mark: 필드 직접변경 테스트 추가

--- src/chapter6/EncapsulateVariable.test.ts
+++ src/chapter6/EncapsulateVariable.test.ts
@@ -1,3 +1,4 @@
+import { defaultOwner } from "./defaultOwner";
 import example from "./EncapsulateVariable";
 
 it("변수 캡슐화 테스트", () => {
@@ -6,4 +7,13 @@ it("변수 캡슐화 테스트", () => {
   example(space);
 
   expect(space.owner).toStrictEqual({ firstName: "마틴", lastName: "파울러" });
+});
+
+it("필드 직접 변경 테스트", () => {
+  const owner1 = defaultOwner();
+  expect(owner1.lastName).toBe("파울러");
+
+  const owner2 = defaultOwner();
+  owner2.lastName = "파슨스";
+  expect(owner1.lastName).toBe("파울러");
 });
