5883e3fae8a6daa30d8c433eb2001b843a9251de
godsenal <tmqps78@gmail.com>
2021-05-04 03:06:08 +0900

    :sparkles: 예제 추가

---
+++ src/chapter7/tempQuery.ts
@@ -0,0 +1,23 @@
+type TItem = {
+  price: number;
+};
+
+class Order {
+  _quantity: number;
+  _item: TItem;
+  constructor(quantity: number, item: TItem) {
+    this._quantity = quantity;
+    this._item = item;
+  }
+
+  get price() {
+    const basePrice = this._quantity * this._item.price;
+    let discountFactor = 0.98;
+
+    if (basePrice > 1000) {
+      discountFactor -= 0.03;
+    }
+
+    return basePrice * discountFactor;
+  }
+}
