d851b033149eca3da7c2c83e3d94ddf7d1e8a51e
godsenal <tmqps78@gmail.com>
2021-05-04 00:50:14 +0900

    :recycle: 2 - 레코드를 클래스로 변경

--- src/chapter7/record.ts
+++ src/chapter7/record.ts
@@ -1,7 +1,14 @@
-const organization = { name: "삼성", country: "KR" };
+class Organization {
+  _data: { name: string; country: string };
+  constructor(data) {
+    this._data = data;
+  }
+}
+
+const organization = new Organization({ name: "삼성", country: "KR" });
 
 const getRawDataOfOrganization = () => {
-  return organization;
+  return organization._data;
 };
 
 const result = `<h1>${getRawDataOfOrganization().name}</h1>`;
