8bf441924fc922cd9d727fc4216d212a5e160a38
godsenal <tmqps78@gmail.com>
2021-05-04 02:56:48 +0900

    :recycle: Priority 클래스가 priority 관련 로직을 수행하도록 적용

--- src/chapter7/primitive.ts
+++ src/chapter7/primitive.ts
@@ -1,10 +1,30 @@
 class Priority {
-  _value: string;
+  _value!: string;
   constructor(value) {
-    this._value = value;
+    if (value instanceof Priority) return value;
+    if (Priority.legalValues().includes(value)) {
+      this._value = value;
+    } else {
+      throw new Error(`<${value}> is invalid for Priority`);
+    }
   }
   toString() {
     return this._value;
+  }
+  get _index() {
+    return Priority.legalValues().findIndex((s) => s === this._value);
+  }
+  static legalValues() {
+    return ["low", "normal", "high", "rush"];
+  }
+  equals(other: Priority) {
+    return this._index === other._index;
+  }
+  higherThen(other: Priority) {
+    return this._index > other._index;
+  }
+  lowerThen(other: Priority) {
+    return this._index < other._index;
   }
 }
 
@@ -12,6 +32,10 @@ class Order {
   _priority: Priority;
   constructor(data) {
     this._priority = data.priority;
+  }
+
+  get priority() {
+    return this._priority;
   }
 
   get priorityString() {
@@ -27,6 +51,6 @@ const orders = [
   new Order({ priority: "rush" }),
 ];
 
-const highPriorityCount = orders.filter(
-  (o) => o.priorityString === "high" || o.priorityString === "rush"
+const highPriorityCount = orders.filter((o) =>
+  o.priority.higherThen(new Priority("normal"))
 ).length;
