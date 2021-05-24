e49e470bcd417f71e9efa5a054d6d596e394d2b2
이태희[Apollo FE Dev] <tae-hee.lee@navercorp.com>
2021-05-24 23:52:35 +0900

    :sparkles: 10.2 예시 추가

---
+++ src/chapter10/ConsolidateConditional.ts
@@ -0,0 +1,15 @@
+class Employee {
+  seniority!: number;
+  monthsDisabled!: number;
+  isPartTime!: number;
+}
+
+const employee = new Employee();
+
+function disabilityAmount() {
+  if (employee.seniority < 2) return 0;
+  if (employee.monthsDisabled > 12) return 0;
+  if (employee.isPartTime) return 0;
+
+  // 나머지
+}
