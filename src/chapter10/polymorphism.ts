744506b6bcf3bb5dbc24a5450fe62990df1d7151
이태희[Apollo FE Dev] <tae-hee.lee@navercorp.com>
2021-05-25 01:28:04 +0900

    :recycle: plumage, speed 인라인화

--- src/chapter10/polymorphism.ts
+++ src/chapter10/polymorphism.ts
@@ -14,19 +14,17 @@ const birds = [
 ] as const;
 
 function plumages(birds) {
-  return new Map(birds.map((b) => [b.name, plumage(b)]));
+  return new Map(
+    birds.map((b) => createBird(b)).map((bird) => [bird.name, bird.plumage])
+  );
 }
 
 function speeds(birds) {
-  return new Map(birds.map((b) => [b.name, airSpeedVelocity(b)]));
-}
-
-function plumage(bird: typeof birds[number]) {
-  return createBird(bird).plumage;
-}
-
-function airSpeedVelocity(bird: typeof birds[number]) {
-  return createBird(bird).airSpeedVelocity;
+  return new Map(
+    birds
+      .map((b) => createBird(b))
+      .map((bird) => [bird.name, bird.airSpeedVelocity])
+  );
 }
 
 function createBird(bird) {
