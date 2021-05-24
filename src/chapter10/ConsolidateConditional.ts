0903f51d209f69e2d818bf1691f5b78e8517974c
이태희[Apollo FE Dev] <tae-hee.lee@navercorp.com>
2021-05-25 00:32:56 +0900

    :recycle: 조건식 추출 - 5

--- src/chapter10/ConsolidateConditional.ts
+++ src/chapter10/ConsolidateConditional.ts
@@ -7,12 +7,14 @@ class Employee {
 const employee = new Employee();
 
 function disabilityAmount() {
-  if (
+  if (isNotEligibleForDisability()) return 0;
+
+  // 나머지
+}
+function isNotEligibleForDisability() {
+  return (
     employee.seniority < 2 ||
     employee.monthsDisabled > 12 ||
     employee.isPartTime
-  )
-    return 0;
-
-  // 나머지
+  );
 }
