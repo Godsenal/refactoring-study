7a7a3db3993dbe78485c2f6897df3612ac750db2
이태희[Apollo FE Dev] <tae-hee.lee@navercorp.com>
2021-05-25 01:19:38 +0900

    :recycle: 여러 함수를 클래스로 묶기

--- src/chapter10/polymorphism.ts
+++ src/chapter10/polymorphism.ts
@@ -22,33 +22,52 @@ function speeds(birds) {
 }
 
 function plumage(bird: typeof birds[number]) {
-  switch (bird.type) {
-    case "europe": {
-      return "노말";
-    }
-    case "africa": {
-      return bird.numberOfCoconuts > 2 ? "지쳤다" : "보통이다";
-    }
-    case "norway": {
-      return bird.voltage > 100 ? "그을렸다" : "예쁘다";
-    }
-    default:
-      return "알 수 없다";
-  }
+  return new Bird(bird).plumage;
 }
 
 function airSpeedVelocity(bird: typeof birds[number]) {
-  switch (bird.type) {
-    case "europe": {
-      return 35;
+  return new Bird(bird).airSpeedVelocity;
+}
+
+class Bird {
+  type!: string;
+  numberOfCoconuts!: number;
+  voltage!: number;
+  isNailed!: boolean;
+
+  constructor(bird) {
+    Object.assign(this, bird);
+  }
+
+  get plumage() {
+    switch (this.type) {
+      case "europe": {
+        return "노말";
+      }
+      case "africa": {
+        return this.numberOfCoconuts > 2 ? "지쳤다" : "보통이다";
+      }
+      case "norway": {
+        return this.voltage > 100 ? "그을렸다" : "예쁘다";
+      }
+      default:
+        return "알 수 없다";
     }
-    case "africa": {
-      return 40 - 2 * bird.numberOfCoconuts;
+  }
+
+  get airSpeedVelocity() {
+    switch (this.type) {
+      case "europe": {
+        return 35;
+      }
+      case "africa": {
+        return 40 - 2 * this.numberOfCoconuts;
+      }
+      case "norway": {
+        return this.isNailed ? 0 : 10 + this.voltage / 10;
+      }
+      default:
+        return null;
     }
-    case "norway": {
-      return bird.isNailed ? 0 : 10 + bird.voltage / 10;
-    }
-    default:
-      return null;
   }
 }
