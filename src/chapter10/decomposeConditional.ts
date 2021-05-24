bf6ddfcf03a0b7e2b6f4e92fd6c7ea6ef2edc902
이태희[Apollo FE Dev] <tae-hee.lee@navercorp.com>
2021-05-24 23:39:34 +0900

    :recycle: 조건문 내부도 함수로 분리

--- src/chapter10/decomposeConditional.ts
+++ src/chapter10/decomposeConditional.ts
@@ -22,10 +22,18 @@ const quantity = 10;
 let charge;
 
 if (summer()) {
-  charge = quantity * plan.summerRate;
+  charge = summerCharge();
 } else {
-  charge = quantity * plan.regularRate + plan.regularServiceCharge;
+  charge = regularCharge();
 }
+function regularCharge(): any {
+  return quantity * plan.regularRate + plan.regularServiceCharge;
+}
+
+function summerCharge(): any {
+  return quantity * plan.summerRate;
+}
+
 function summer() {
   return !date.isBefore(plan.summerStart) && !date.isAfter(plan.summerEnd);
 }
